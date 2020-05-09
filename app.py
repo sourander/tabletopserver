14ec6cb519652ed8391b69c11c98c28a72bd0c54
Jani Sourander <sourander@gmail.com>
2020-05-09 08:58:41 +0300

    Initial version of raiders checkbox

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 from config import Config
 from views.raidersofthenorthsea import raidersofthenorthsea
--- views/raidersofthenorthsea.py
+++ views/raidersofthenorthsea.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, request
 
 raidersofthenorthsea = Blueprint('raiders_of_the_north_sea', __name__,)
 
-@raidersofthenorthsea.route('/raiders')
+@raidersofthenorthsea.route('/raiders', methods=['GET', 'POST'])
 def raiders():
-    return render_template("raiders.html")
+
+    extensions = [('hall_of_heroes', 'Hall of Heroes'),
+                  ('field_of_fame', 'Fields of Fame')]
+    tokens = []
+    if request.method == 'POST':
+        addons = request.form.getlist('my_checkbox')
+        print(addons)
+        tokens = ['gold', 'stone']
+
+
+    return render_template("raiders.html", extensions=extensions, tokens=tokens)
