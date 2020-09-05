5d1227d64152f4921b1ebcb14957b7d033f0a672
Jani Sourander <sourander@gmail.com>
2020-09-05 14:31:45 +0300

    Add OG metadata

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 from config import Config
 from views.raidersofthenorthsea import raidersofthenorthsea
+
 
 app = Flask(__name__)
 
@@ -12,9 +13,7 @@ app.register_blueprint(raidersofthenorthsea)
 @app.route("/")
 def portal():
     links = [('raiders', 'Raiders of the North Sea')]
-
     return render_template("portal.html", links=links)
-
 
 if __name__ == '__main__':
     app.run()
